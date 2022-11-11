5bb0ce0048b40c45332b395f250fd6a158132352
Juan Ventrone <j.ventrone78@outlook.es>
2022-11-11 17:58:43 -0400

    ya esta listo el prototipo

--- init.py
+++ init.py
@@ -1,32 +1,35 @@
+import configparser
 import logging
-# import pandas as pd
-import telegram
-import numpy as np
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Updater, CommandHandler, CallbackContext
-import requests
 
+import numpy as np
+import prettytable as pt
+import requests
+import telegram
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode
+from telegram.ext import CallbackContext, CommandHandler, Updater
 
 # Enable logging
+config = configparser.ConfigParser()
+config.read('conf.ini')
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
-bot = telegram.Bot(token='5571349214:AAHCWNDBusMF7MYjhJ-QXjpSBNRqcyDzyX8')
+bot = telegram.Bot(token=config['KEY']['token'])
 try:
     chat_id = bot.get_updates()[-1].message.chat_id
 except IndexError:
     chat_id = 0
 ####################################################################################################################
 
-async def blockchain_stats():
+def blockchain_stats():
     # Info Blockchain Stats
     url_pool = "https://api.blockchain.info/stats"
-    r = await requests.get(url_pool)
+    r = requests.get(url_pool)
     return r.json()
 
 def pool_stat():
     # Info Pool Stats
-    url_pool = "https: // api.blockchain.info/pools?timespan = 7 days"
+    url_pool = "https://api.blockchain.info/pools?timespan=7days"
         
     r = requests.get(url_pool)
     return r.json()
@@ -41,14 +44,14 @@ def ThConstProfit():
     return binance_calc
 
 
-async def currency_international_price(update):
+def inter_price(update, context):
     # Get diferent BTC prices diferent currency.
-    url_pool = "https: // blockchain.info/ticker"
-    r = await requests.get(url_pool)
-    json_prices = r.json()
-    prices = [str(i["symbol"] + i["15m"]) for i in json_prices]
-    update.message.reply_text('International prices:'
-                              +str( [(i+"\n")  for i in prices]))
+    url_pool = "https://blockchain.info/ticker"
+    r = requests.get(url_pool)
+    prices = r.json()
+    tittles = ['-', 'Unidad', 'Simbolo']
+    data = [('BTC-'+ i, prices[i]['last'], i) for i in prices]
+    send_table(update, CallbackContext, tittles, data)
     
     
 
@@ -66,70 +69,102 @@ def start(update, context: CallbackContext):
         'Mayor Informacion de este Bot /info\n' +
         '¿Quienes somos? /soporte'
     )
+    calc(update, context)
 
-def total_stat(update):
+def general_stat():
     stat = blockchain_stats()
     thProfit = ThConstProfit()
+    tittles = ['', 'Unidad', 'Simbolo']
     volumen_perBlock = stat['trade_volume_usd'] / \
         stat['n_blocks_mined']
-        
-    update.message.reply_text(
-        'Precio Hash-USD: '+ str(thProfit)+'\n'
-        'Precio BTC-USD: ' + str(stat['market_price_usd'])+'\n'
-        'Hashrate: ' + str(stat['hash_rate'])+'\n'
-        'Dificultad: ' + str(stat['difficulty'])+'\n'
-        'Siguiente Bloque: ' + str(stat['nextretarget'])+'\n'
-        'Minutos entre Bloques: ' + str(stat['minutes_between_blocks'])+'\n'
-        'Estimado Volumen de Transacción en USD: ' +
-        str(stat['estimated_transaction_volume_usd'])+'\n'
-        'Minutos entre Bloques: ' + str(stat['minutes_between_blocks'])+'\n'
-        'Volumen USD por Bloque: ' +
-        str(volumen_perBlock)+'\n'
-        )
-    
+    TH = thProfit/0.00000001
+    profit_THUSD = thProfit*stat['market_price_usd']
+    data = [
+        ('Precio BTC-USD', stat['market_price_usd'], '$'),
+        ('ProfitDay TH-SAT', round(TH), 'SAT'),
+        ('ProfitDay TH-USD', profit_THUSD, 'TH/$'),
+        ('Hashrate', stat['hash_rate'], 'TH/s'),
+        ('Dificultad', stat['difficulty'], ''),
+        ('Sig. Bloque', stat['nextretarget'], 'BLock'),
+        ('Tiempo entre Bloques', stat['minutes_between_blocks'], 'Min'),
 
+        ('Vol-Trans USD',
+         stat['estimated_transaction_volume_usd'], '$', ),
+
+        ('Vol-USD por Bloque', volumen_perBlock, '$')
+
+    ]
+    return tittles, data
+
+def total_stat(update, context):
+    tittles, data = general_stat()
+    send_table(update, CallbackContext, tittles, data)
+
+def min_stat(update, context):
+    tittles, data = general_stat()
+    logs = [(i + ' - ' + f'{unit:,.2f}' + ' ' + symbol + ' \n\n')
+            for i, unit, symbol in data]
+    c = ''
+    for i in logs:
+        c = c + i
+    update.message.reply_text( c )
+
+def profit_pool(update, context):
+    pool=pool_stat()
+    data = [(i, pool[i], 'in 7 Days') for i in pool]
+    tittles = ['Pools', 'Bloques Minados', 'Tiempo']
+    send_table(update, CallbackContext, tittles, data)
+    
+def send_table(update: Updater, context: CallbackContext,tittles, data):
+    table = pt.PrettyTable(tittles)
+    table.align[tittles[0]] = 'l'
+     
+    for item, units, symbol in data:
+        table.add_row([item, f'{units:,.2f}', symbol])
+
+    update.message.reply_text(f'<pre>{table}</pre>', parse_mode=ParseMode.HTML)
+    
+    
 def info(update, context):
     update.message.reply_text(
-        'Precio BTC: Precio Actual BTC-USD \n\n' +
-        'Precio Internacional: Precio Actual BTC respecto diferentes Monedas \n'
-        'MinerStat: Estadistica total, Hashrate, Bloques Minados, Siguiente Bloque, Ultimo bloque, Dificultad, etc...\n'
-        'PoolStat: Numero de Bloques minado en 7 dias \n'
+        'Estadistica General para Mineros, las tablas solo seran reflejadas en desktop'
         )
-def calc(update):
+
+def calc(update, context):
     
-    keyboard = [[InlineKeyboardButton(text='⚒🖥 VS ⚡🔌',
-                                      url='https://t.me/LatamStatBot')],]
+    keyboard = [[InlineKeyboardButton(text='⚒🖥   VS   ⚡🔌',
+                                      url='https://t.me/Hosting_calc_Bot')], ]
     reply_markup = InlineKeyboardMarkup(keyboard)
 
     update.message.reply_text(
         'Calcula EL rendimiento de tus TH  "VS""  el consumo electrico', reply_markup=reply_markup
     )
-def usd_price(update):
-    stat = blockchain_stats()
-    update.message.reply_text( '$'+  str(stat['market_price_usd']) )
 
+
+def usd_price(update, context):
+    stat = blockchain_stats()
+    price = stat['market_price_usd']
+    update.message.reply_text('Precio BTC-USD - $' + f'{price:,.2f}')
+    
 def main():
 
     # Create the Updater and pass it your bot's token.
     # Make sure to set use_context=True to use the new context based callbacks
     # Post version 12 this will no longer be necessary
-    updater = Updater(
-        '5571349214:AAHCWNDBusMF7MYjhJ-QXjpSBNRqcyDzyX8', use_context=True)
+    updater = Updater( config['KEY']['token'], use_context=True)
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
 
-    # on different commands - answer in Telegram
-    # p.add_handler(CommandHandler("menu", menu))
     dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("estadisticas General", total_stat))
+    dp.add_handler(CommandHandler("tablestat", total_stat))
+    dp.add_handler(CommandHandler("textstat", min_stat))
+    dp.add_handler(CommandHandler("tableinterprice", inter_price))
+    dp.add_handler(CommandHandler("usdprice", usd_price))
+    dp.add_handler(CommandHandler("tablepoolstat", profit_pool))
     dp.add_handler(CommandHandler("soporte", soporte))
     dp.add_handler(CommandHandler("info", info))
-    dp.add_handler(CommandHandler("precio int.", currency_international_price))
-    dp.add_handler(CommandHandler("precio", usd_price))
-    p.add_handler(CommandHandler("calc", calc))
-    # dp.add_handler(MessageHandler(Filters.sticker, sticker))
-    # dp.add_handler(MessageHandler(Filters.text, echo))
+    
     # Start the Bot
     updater.start_polling()
 
